7a37178ec8cba8e9ef8f02435bbf70d203f54132
Pabitra18 <pabitrachakraborty4u@gmail.com>
2026-01-25 11:52:47 +0530

    Pipeline upgraded

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -112,8 +112,8 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
-        params = {}  # Hardcoded empty dict for now
+        params = load_params(params_path='params.yaml')
+       # params = {}  # Hardcoded empty dict for now
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -123,11 +123,11 @@ def main():
         metrics = evaluate_model(clf, X_test, y_test)
 
         # Experiment tracking using dvclive
-        # with Live(save_dvc_exp=True) as live:
-        #     live.log_metric('accuracy', accuracy_score(y_test, y_test))
-        #     live.log_metric('precision', precision_score(y_test, y_test))
-        #     live.log_metric('recall', recall_score(y_test, y_test))
-        #     live.log_params(params)
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+            live.log_params(params)
         
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
